da101f46c4ca8d146e30cd7822a1d711960fceb6
No2Ross <78097380+No2Ross@users.noreply.github.com>
2024-06-02 16:25:00 +0100

    Start shared dyngen simulation

--- TrAGEDy_paper_code/Dyngen_simulations/start_shared/start_shared_Dyngen_simulate.R
+++ TrAGEDy_paper_code/Dyngen_simulations/start_shared/start_shared_Dyngen_simulate.R
@@ -1,5 +1,12 @@
 library(tidyverse)
 library(dyngen)
+library(dyno)
+library(unixtools)
+
+#set.tempdir("pooling-scRNA/")
+ulimit::memory_limit(50000)
+
+setwd("/datastore/Ross/TrAGEDy_V2")
 
 set.seed(2)
 
@@ -8,10 +15,10 @@ backbone <- backbone_linear()
 config <-
   initialise_model(
     backbone = backbone,
-    num_cells = 2000,
+    num_cells = 1500,
     num_tfs = nrow(backbone$module_info),
-    num_targets = 300,
-    num_hks = 300,
+    num_targets = 250,
+    num_hks = 150,
     simulation_params = simulation_default(
       census_interval = 10, 
       ssa_algorithm = ssa_etl(tau = 300 / 3600),
@@ -42,14 +49,14 @@ plot_backbone_statenet(model_common)
 model_common$module_network
 
 m4_genes <- model_common$feature_info %>% filter(module_id %in% c("Burn1")) %>% pull(feature_id)
-b3_genes <- model_common$feature_info %>% filter(module_id %in% c("B2")) %>% pull(feature_id)
+b5_genes <- model_common$feature_info %>% filter(module_id %in% c("B5")) %>% pull(feature_id)
 
 model_ko <- model_common
 model_ko$simulation_params$experiment_params <- simulation_type_knockdown(
   num_simulations = 300,
   timepoint = 0, 
-  genes = b3_genes,
-  num_genes = length(b3_genes),
+  genes = b5_genes,
+  num_genes = length(b5_genes),
   multiplier = 0,
 )
 
@@ -78,5 +85,14 @@ model_comb <-
 plot_gold_mappings(model_comb, do_facet = FALSE)
 plot_simulations(model_comb)
 
+plot_heatmap(model_wt, features_oi = 50, trajectory = T)
+
 WT <- as_seurat(model_wt)
 KO <- as_seurat(model_ko)
+
+library(dyno)
+
+saveRDS(WT, "simulation/start_shared/start_shared_wt.rds")
+saveRDS(KO, "simulation/start_shared/start_shared_ko.rds")
+
+
